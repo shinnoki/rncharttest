e2c7f1a50a59adf9d4ce7960977582de73a1382d
Yudai <y.shinnoki@gmail.com>
2017-11-18 00:16:16 +0900

    implemented SimpleChart

--- App.js
+++ App.js
@@ -1,57 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+import { StyleSheet, View } from 'react-native';
+import Chart from 'react-native-chart';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  chart: {
+    width: 200,
+    height: 200,
   },
 });
+
+const data = [[
+  [0, 1],
+  [1, 3],
+  [3, 7],
+  [4, 9],
+]];
+
+class SimpleChart extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Chart
+          style={styles.chart}
+          data={data}
+          verticalGridStep={5}
+          type="line"
+          showDataPoint={true}
+          color={['#e1cd00']}
+         />
+      </View>
+    );
+  }
+}
+
+export default SimpleChart;
